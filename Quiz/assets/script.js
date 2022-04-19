987ddc17a232890c1c2b9952b68687455257bf37
Delta <dhriti.baruah20@gmail.com>
2022-04-19 20:33:40 +0530

    initial

--- Quiz/assets/script.js
+++ Quiz/assets/script.js
@@ -41,11 +41,32 @@ const questions = [
 ];
 
 
+// TIMER
+// let demo = document.getElementById("demo")
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2024 15:37:25").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
-// question.innerText = questions[questionsText][0]
-questions.map(question => {
-  const singleques = document.querySelector("#questions")
-const options = document.querySelector("#options")
-  singleques = questions.questionText
-  options = questions.options
-})
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  demo.innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    demo.innerHTML = "EXPIRED";
+  }
+}, 1000);
