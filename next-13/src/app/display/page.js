65e592fb3f6a91136d8f6053cb49956eccb45505
Dhriti <dhriti.baruah20@gmail.com>
2023-05-27 08:50:53 +0530

    mongoose setUP

---
+++ next-13/src/app/display/page.js
@@ -0,0 +1,12 @@
+import '../../app/globals.css'
+import React from 'react'
+
+export default async function Display(){
+
+ 
+  return (
+    <div className='container'>
+   <h1>Display</h1>
+</div>
+  )
+}
--- next-13/src/app/page.js
+++ next-13/src/app/page.js
@@ -1,95 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
+const mongoose = require('mongoose')
+const Todos = require('../../model/Todo')
 
-export default function Home() {
+export default async function Home() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/nextJSCRUD',{
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    const todos = await Todos.find()
+    console.log(todos)
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div className='container'>
+    <table className="table">
+        <thead>
+            <tr>
+                <th scope="col">Title</th>
+                <th scope="col">Todo</th>
+                <th scope="col">Options</th>
+            </tr>
+        </thead>
+        <tbody>
+            {todos.map((element)=>{
+               return(
+                <tr key={element._id}>
+                <td>{element.title}</td>
+                <td>{element.todo}</td>
+                <td>
+                    <button className="btn btn-danger" >Delete</button>
+                    <button className="btn btn-primary">Edit</button>
+                </td>
+            </tr>
+               ) 
+            })}                      
+        </tbody>
+    </table>
+</div>
   )
 }
