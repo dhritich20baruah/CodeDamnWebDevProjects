2e2d39e4e97a3f312c31bce80dd9a9f7c7e794ec
Dhriti <dhriti.baruah20@gmail.com>
2023-06-15 18:30:24 +0530

    assertion function

--- TypeScript/src/advanced.ts
+++ TypeScript/src/advanced.ts
@@ -43,3 +43,59 @@ class Point2D  {
         this.y = Math.random()
     }
 }
+
+//User defined Type Guards
+type Square = {
+    size: number,
+}
+
+type Rectangle = {
+    width: number,
+    height: number,
+}
+
+type Shape = Square | Rectangle
+
+function isSquare(shape: Shape): shape is Square{
+    return 'size' in shape;
+}
+
+function isRectangle(shape: Shape): shape is Rectangle{
+    return 'width' in shape
+}
+
+function area(shape: Shape){
+    if (isSquare(shape)){
+        return shape.size * shape.size
+    }
+
+    if (isRectangle(shape)){
+        return shape.width * shape.height
+    }
+
+    const _ensure: never = shape;
+    return _ensure
+}
+
+//assertion function
+type Person = {
+    name: string,
+    dateOfBirth?: Date
+}
+
+function assert(condition: unknown, message: string): asserts condition {
+    if(!condition) throw new Error(message)
+}
+
+function assertDate(value: unknown): asserts value is Date {
+    if(value instanceof Date) return;
+    else throw new TypeError('value is not a Date')
+}
+
+const maybePerson = loadPerson()
+
+assert(maybePerson != null, 'Could not load person')
+console.log('Name:', maybePerson.name)
+
+assertDate(maybePerson.dateOfBirth)
+console.log('Date of Birth', maybePerson.dateOfBirth.toISOString())
