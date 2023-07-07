7784ad04cdfedc81fb5f555c6581d2e68663cbcd
Dhriti <dhriti.baruah20@gmail.com>
2023-07-07 23:37:16 +0530

    Login setup

---
+++ next/next_app/src/app/Login/page.js
@@ -0,0 +1,26 @@
+"use client"
+import React from "react";
+import { useSession, signIn, signOut } from "next-auth/react";
+
+const Login = () => {
+    const {data: session} = useSession()
+
+    if(session){
+        return(
+            <div>
+                <p>Welcome, {session.user.email}</p>
+                <button onClick={()=>signOut()}>Sign Out</button>
+            </div>
+            )
+        }
+    else{
+        return(
+            <div>
+                <p>You are not Signed In</p>
+                <button onClick={()=>signIn()}>Sign In</button>
+            </div>
+        )
+    }
+}
+
+export default Login
